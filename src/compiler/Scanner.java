b560550efcafc5cee638e98e9d3e9ff7df4bd113
David Marne <davemarne@gmail.com>
2014-01-27 21:21:34 -0700

    updated layout

--- src/compiler/Scanner.java
+++ src/compiler/Scanner.java
@@ -1,5 +1,39 @@
 package compiler;
 
-public class Scanner {
 
+public class Scanner {
+	Token currentToken;
+	String currentLexeme;
+	int lineNumber;
+	int colNumber;
+	
+	public static Token getToken(){
+		return dispatch();
+	}
+	
+	public static Token dispatch(){
+		//skip white space
+		//peek
+		//switch for each possible FSA
+		//call that FSA's fucntion which returns token
+	}
+	
+	public static openFile(String fileName){
+		//get file frome esus
+		
+	}
+	
+	public static String getLexeme(){
+		return currentLexeme;
+	}
+	
+	public static int getLineNumber(){
+		return lineNumber;
+	}
+	
+	public static int getColumnNumber(){
+		return colNumber;
+	}
+	
+	
 }
