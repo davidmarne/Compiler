3060eb000908cc0a464df1c3e31d5c8cfadd8467
Dominic Frost <frostationdevice@gmail.com>
2014-03-03 14:24:32 -0700

    added Token.. oops

---
+++ src/compiler/Token.java
@@ -0,0 +1,17 @@
+package compiler;
+
+public class Token {
+
+	public String token;
+	public String lexeme;
+	public int lineNumber;
+	public int colNumber;
+	
+	public Token(String token, String lexeme, int lineNumber, int colNumber) {
+		this.token = token;
+		this.colNumber = colNumber;
+		this.lexeme = lexeme;
+		this.lineNumber = lineNumber;
+		
+	}
+}
