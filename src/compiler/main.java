339ffa8fb56313669d8049c14a6c6bfdb5f3d225
Dominic Frost <frostationdevice@gmail.com>
2014-03-03 14:12:05 -0700

    refactored the scanner file to create token objects so we can store all the information about the objects

--- src/compiler/main.java
+++ src/compiler/main.java
@@ -7,19 +7,19 @@ public class main {
 
 	public static void main(String[] args) {
 		//String filename = args[0];
-		ArrayList<String> tkns = new ArrayList<String>();
+		ArrayList<Token> tkns = new ArrayList<Token>();
 		try{
 			Scanner.openFile("test.txt");
-			String s = "";
+			Token s = null;
 			while(true){
 				try {
 					s = Scanner.getToken();
 					tkns.add(s);
-					System.out.format("token: %-15s", Scanner.getTokenName());
-					System.out.format("  line number: %-3s", Scanner.getLineNumber());
-					System.out.format("  column number: %-3s", Scanner.getColumnNumber());
-					System.out.println("  lexeme " + Scanner.getLexeme());
-					if(s == "MP_EOF") {
+					System.out.format("token: %-15s", s.token);
+					System.out.format("  line number: %-3s", s.lineNumber);
+					System.out.format("  column number: %-3s", s.colNumber);
+					System.out.println("  lexeme " + s.lexeme);
+					if(s.token == "MP_EOF") {
 						break;
 					}
 				} catch (Exception e) {
